9c535d6fc1bb29181eee889ec39e69d120ce9eef
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-09 23:54:11 +0100

    Update and rename quests.html to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".btn-primary").addEventListener("click", () => {
+    console.log("Get Started");
+});
+
+document.querySelector(".btn-secondary").addEventListener("click", () => {
+    console.log("Login");
+});
